b4e95c82d02e9165b5aae884c547d4b78a60d32a
Robert M. Lefkowitz <r0ml@mac.com>
2021-07-24 13:39:15 -0400

    added a shell for document framework -- thinking about making this a document app

---
+++ MetalTinkerTests/MetalTinkerTests.swift
@@ -0,0 +1,29 @@
+// Copyright (c) 1868 Charles Babbage
+// Found amongst his effects by r0ml
+
+import XCTest
+@testable import MetalTinker
+
+class MetalTinkerTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
